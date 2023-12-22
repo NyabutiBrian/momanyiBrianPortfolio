9faca3bc62c59229de536cf770b36caf9d71a194
Momanyi Poet <momanyiturner@gmail.com>
2023-12-22 19:25:46 +0300

    tailwind source code

--- blueprints/build/script.js
+++ blueprints/build/script.js
@@ -49,3 +49,117 @@ window.addEventListener("scroll", function(){
   currentScroll = this.window.pageYOffset;
 });
 
+// OVERLAY EFFECT (WELCOME)
+TweenMax.to("#overlay", 2, {
+  top: "-120%",
+  delay: 2,
+  ease: "Expo.easeInOut"
+});
+
+// OVERLAY EFFECT (PAGES)
+TweenMax.to("#overlay-page", 2, {
+  top: "-120%",
+  delay: 0,
+  ease: "Expo.easeInOut"
+});
+
+// OVERLAY APPEAR ONLY WHEN PAGE IS REFRESHED
+document.addEventListener("DOMContentLoaded", function() {
+  if (performance.navigation.type === 1) {
+    // Page is being loaded for the first time or refreshed
+    // Show the overlay
+    document.getElementById("overlay").style.display = "flex";
+  } else {
+    // Page is being navigated to
+    // Do not show the overlay
+    document.getElementById("overlay").style.display = "none";
+  }
+});
+
+// OVERLAY APPEAR ONLY WHEN PAGE IS NAVIGATED TO
+document.addEventListener("DOMContentLoaded", function() {
+  if (performance.navigation.type === 1) {
+    // Page is being loaded for the first time or refreshed
+    // DO not Show the overlay
+    document.getElementById("overlay-page").style.display = "none";
+  } else {
+    // Page is being navigated to
+    // show the overlay
+    document.getElementById("overlay-page").style.display = "flex";
+  }
+});
+
+// UPWARD EFFECT MOVING UP
+TweenMax.from(".up-effect", 1, {
+  y: 50,
+  delay: 0.6,
+  opacity: 0,
+  ease: "Expo.easeInOut"
+});
+
+// MAGNETIC BUTTON
+const magneticButtons = document.querySelectorAll('.magneticButton');
+
+magneticButtons.forEach(magneticButton => {
+  magneticButton.addEventListener('mouseenter', function () {
+    gsap.to(magneticButton, { duration: 0.3, scale: 1, ease: 'power2.out' });
+    document.addEventListener('mousemove', moveButton);
+  });
+
+  magneticButton.addEventListener('mouseleave', function () {
+    gsap.to(magneticButton, { duration: 0.3, scale: 1, ease: 'power2.out' });
+    document.removeEventListener('mousemove', moveButton);
+    gsap.to(magneticButton, { duration: 0.3, x: 0, y: 0, ease: 'power2.out' });
+  });
+
+  function moveButton(e) {
+    const rect = magneticButton.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    gsap.to(magneticButton, { duration: 0.2, x: x, y: y, ease: 'power2.out' });
+  }
+});
+
+// IMAGE POP UP
+const imageContainers = document.querySelectorAll('.hover-text');
+
+imageContainers.forEach(imageContainer => {
+  const popUpImage = imageContainer.querySelector('.hover-img');
+
+  imageContainer.addEventListener('mouseenter', function () {
+    gsap.to(popUpImage, { duration: 0.6, opacity: 1, scale: 1.1, ease: 'power2.out' });
+  });
+
+  imageContainer.addEventListener('mouseleave', function () {
+    gsap.to(popUpImage, { duration: 0.6, opacity: 0, scale: 1, ease: 'power2.out' });
+  });
+});
+
+// SHOWING FOOTER
+let mybutton = document.getElementById("footer-sec");
+
+// When the user scrolls down 600px from the top of the document, show the footer
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// SMOOTH SCROLL
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+  wrapper: '#smooth-wrapper',
+  content: '#smooth-content',
+  smooth: 8,
+})
